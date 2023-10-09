2672acc5705f9ae7b016eca0864e99f11afa461a
Mohammad Abdel Hadi <mohammad.a@echo-tech.com>
2023-10-09 16:36:57 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Register_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
